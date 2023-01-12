62fb995c9f010a7956f1b4d27608797c9bf79f94
Marwan <marwan.elbahnasawy@gmail.com>
2023-01-12 21:58:37 +0200

    Added some empty fragments

--- app/src/main/java/com/example/foodplanner/MainActivity.java
+++ app/src/main/java/com/example/foodplanner/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.foodplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this,navController);
+
     }
 }
