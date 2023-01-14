62a96dc79041541ce52bdaa1153bd5dbe434c358
Marwan <marwan.elbahnasawy@gmail.com>
2023-01-14 17:12:36 +0200

    Enabled logging with username and password

--- app/src/main/java/com/example/foodplanner/Controller/Fragments/InitialFragments/Register.java
+++ app/src/main/java/com/example/foodplanner/Controller/Fragments/InitialFragments/Register.java
@@ -1,5 +1,6 @@
 package com.example.foodplanner.Controller.Fragments.InitialFragments;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -8,6 +9,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,16 +18,19 @@ import android.widget.Toast;
 import com.example.foodplanner.Controller.Activities.MainActivity;
 import com.example.foodplanner.R;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
 
 public class Register extends Fragment {
-    TextInputEditText signUp_email,signUp_password;
+    TextInputEditText signUp_email, signUp_password;
     AppCompatButton register;
     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
+    private ProgressDialog loadingBar;
 
 
     @Override
@@ -44,60 +49,74 @@ public class Register extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
-        signUp_email=view.findViewById(R.id.signUp_email_edt);
-        signUp_password=view.findViewById(R.id.signUp_password_edt);
-        register=view.findViewById(R.id.register_button);
         super.onViewCreated(view, savedInstanceState);
+
+        signUp_email = view.findViewById(R.id.signUp_email_edt);
+        signUp_password = view.findViewById(R.id.signUp_password_edt);
+        register = view.findViewById(R.id.register_button);
+        loadingBar = new ProgressDialog(requireContext());
+
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-               String email= signUp_email.getText().toString();
-                String password= signUp_password.getText().toString();
+                loadingBar.setTitle("Registering");
+                loadingBar.setMessage("Please wait while signing in");
+                loadingBar.setCanceledOnTouchOutside(false);
 
 
-                if((!email.isEmpty())&&(!password.isEmpty())) {
+                String email = signUp_email.getText().toString();
+                String password = signUp_password.getText().toString();
+
+
+                if ((!email.isEmpty()) && (!password.isEmpty())) {
+
+                    loadingBar.show();
+
                     firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+
+                        loadingBar.dismiss();
 
 
                         if (task.isSuccessful()) {
                             FirebaseUser user = firebaseAuth.getCurrentUser();
                             if (user != null) {
                                 updateUserData(user, email);
-                                //Navigation.findNavController(view).navigate(RegisterDirections.actionNavRegisterToNavHome());
-//                                Intent intent = new Intent(requireContext(),MainActivity.class);
-//                                startActivity(intent);
+                                Intent intent = new Intent(requireContext(), MainActivity.class);
+                                startActivity(intent);
                             }
                         } else {
                             Exception exception = task.getException();
                             if (exception == null) {
-                                Toast.makeText(getContext(), "UnExpected error occurred", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getContext(), "UnExpected error occurred", Toast.LENGTH_LONG).show();
                             } else {
-                                Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                if (((FirebaseAuthException) exception).getErrorCode().equals("ERROR_WEAK_PASSWORD")) {
+
+                                    Toast.makeText(getContext(), "Password should be at least 6 characters", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(getContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                                }
+
                             }
 
                         }
 
                     });
-                }
-                else {
-                    if(email.isEmpty())
-                    {
-                        Toast.makeText(getContext(),"Enter your email",Toast.LENGTH_SHORT).show();
-                    }
-                    else if (password.isEmpty())
-                    {
-                        Toast.makeText(getContext(),"Enter your password",Toast.LENGTH_SHORT).show();
+                } else {
+                    if (email.isEmpty()) {
+                        Toast.makeText(getContext(), "Enter your email", Toast.LENGTH_SHORT).show();
+                    } else if (password.isEmpty()) {
+                        Toast.makeText(getContext(), "Enter your password", Toast.LENGTH_SHORT).show();
                     }
 
                 }
 
-                Intent intent = new Intent(requireContext(), MainActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(requireContext(), MainActivity.class);
+//                startActivity(intent);
 
 
             }
+
             private void updateUserData(FirebaseUser currentUser, String name) {
                 UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
                         .setDisplayName(name)
@@ -110,10 +129,10 @@ public class Register extends Fragment {
 
                     } else {
                         Exception exception = task.getException();
-                        if (exception == null){
-                            Toast.makeText(getContext(),"UnExpected error occurred",Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(getContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        if (exception == null) {
+                            Toast.makeText(getContext(), "UnExpected error occurred", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
--- app/src/main/java/com/example/foodplanner/Controller/Fragments/InitialFragments/SignIn.java
+++ app/src/main/java/com/example/foodplanner/Controller/Fragments/InitialFragments/SignIn.java
@@ -1,25 +1,40 @@
 package com.example.foodplanner.Controller.Fragments.InitialFragments;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.foodplanner.Controller.Activities.MainActivity;
 import com.example.foodplanner.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
 
 public class SignIn extends Fragment {
 
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     Button button;
-
-
+    TextView et_email;
+    TextView et_password;
+    Button btn_signIn;
+    private ProgressDialog loadingBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,14 +54,74 @@ public class SignIn extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         button = view.findViewById(R.id.buttonSignIn);
+        et_email = view.findViewById(R.id.et_email);
+        et_password = view.findViewById(R.id.et_password);
+        btn_signIn = view.findViewById(R.id.btn_signIn);
+        loadingBar = new ProgressDialog(requireContext());
+
+        btn_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadingBar.setTitle("Signing in");
+                loadingBar.setMessage("Please wait while signing in");
+                loadingBar.setCanceledOnTouchOutside(false);
+
+
+                String email = et_email.getText().toString();
+                String password = et_password.getText().toString();
+
+
+                if ((!email.isEmpty()) && (!password.isEmpty())) {
+
+                    loadingBar.show();
+
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            loadingBar.dismiss();
+
+                            if (task.isSuccessful()) {
+                                Toast.makeText(requireContext(), "login successful", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(requireContext(), MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Exception exception = task.getException();
+                                if (exception == null) {
+                                    Toast.makeText(getContext(), "UnExpected error occurred", Toast.LENGTH_LONG).show();
+                                } else {
+                                    if (((FirebaseAuthException) exception).getErrorCode().equals("ERROR_USER_NOT_FOUND")) {
+                                        Toast.makeText(getContext(), "User not found", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+
+                                }
+
+                            }
+                        }
+                    });
+                } else {
+                    if (email.isEmpty()) {
+                        Toast.makeText(getContext(), "Enter your email", Toast.LENGTH_SHORT).show();
+                    } else if (password.isEmpty()) {
+                        Toast.makeText(getContext(), "Enter your password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Navigation.findNavController(view).navigate(SignInDirections.actionNavSignInToNavRegister());
+
             }
         });
+
     }
+
 
 }
