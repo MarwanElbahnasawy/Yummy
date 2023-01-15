f53d850046e58ef5ed2c92c188dd83b2ea26ffe6
Marwan <marwan.elbahnasawy@gmail.com>
2023-01-15 19:22:27 +0200

    Enabled logging with google account

--- app/src/main/java/com/example/foodplanner/Controller/Fragments/InitialFragments/SpashScreen.java
+++ app/src/main/java/com/example/foodplanner/Controller/Fragments/InitialFragments/SpashScreen.java
@@ -11,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.window.SplashScreen;
 
 import com.example.foodplanner.R;
 
