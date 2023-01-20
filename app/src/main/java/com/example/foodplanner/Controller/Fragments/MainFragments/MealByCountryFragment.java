abcb02bb2d5e9fce5634730c77be31f2f7ecac23
esraa2011 <esraa4255@gmail.com>
2023-01-21 01:14:28 +0200

    Meals By Countries Ui

---
+++ app/src/main/java/com/example/foodplanner/Controller/Fragments/MainFragments/MealByCountryFragment.java
@@ -0,0 +1,23 @@
+package com.example.foodplanner.Controller.Fragments.MainFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.foodplanner.R;
+
+public class MealByCountryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_meal_by_country, container, false);
+    }
+}
