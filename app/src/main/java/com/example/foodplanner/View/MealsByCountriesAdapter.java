bfeb60d158369e361401048ef9749e816773aaad
esraa2011 <esraa4255@gmail.com>
2023-01-25 01:08:08 +0200

    meal details in search by country

--- app/src/main/java/com/example/foodplanner/View/MealsByCountriesAdapter.java
+++ app/src/main/java/com/example/foodplanner/View/MealsByCountriesAdapter.java
@@ -7,19 +7,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.foodplanner.Controller.Fragments.MainFragments.MealByCategoryFragmentDirections;
+import com.example.foodplanner.Controller.Fragments.MainFragments.MealByCountryFragmentDirections;
 import com.example.foodplanner.Model.MealsItem;
+import com.example.foodplanner.Model.Root;
+import com.example.foodplanner.Network.RetrofitClient;
 import com.example.foodplanner.R;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MealsByCountriesAdapter  extends RecyclerView.Adapter<MealsByCountriesAdapter.MyViewHolder> {
     List<MealsItem> meals;
+    ViewGroup viewGroup;
 
     public MealsByCountriesAdapter(List<MealsItem> meals) {
         this.meals=meals;
@@ -28,7 +37,7 @@ public class MealsByCountriesAdapter  extends RecyclerView.Adapter<MealsByCountr
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
+        viewGroup=parent;
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_meal_items, parent, false);
         return new MealsByCountriesAdapter.MyViewHolder(view);
     }
@@ -37,6 +46,36 @@ public class MealsByCountriesAdapter  extends RecyclerView.Adapter<MealsByCountr
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         Glide.with(holder.itemView).load(meals.get(position).getStrMealThumb()).into(holder.mealImage);
         holder.mealName.setText(meals.get(position).getStrMeal());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RetrofitClient.getInstance().getMyApi().getMealById(Integer.parseInt(meals.get(position).getIdMeal()))
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Observer<Root>() {
+                            @Override
+                            public void onSubscribe(@io.reactivex.rxjava3.annotations.NonNull Disposable d) {
+                                // loading ui
+                            }
+
+                            @Override
+                            public void onNext(@io.reactivex.rxjava3.annotations.NonNull Root root) {
+                                Navigation.findNavController(viewGroup).navigate(MealByCountryFragmentDirections.actionMealByCountryFragmentToMealDeatailsFragment(root.getMeals().get(0)));
+                            }
+
+                            @Override
+                            public void onError(@io.reactivex.rxjava3.annotations.NonNull Throwable e) {
+
+                            }
+
+                            @Override
+                            public void onComplete() {
+
+                            }
+                        });
+
+            }
+        });
 
     }
 
